be2e313ad71ab8455adf6dac4d61ef0fa562c993
Hanamichidesu <fuchu.1226@gmail.com>
2025-12-10 19:34:38 +0900

    Initial Commit

---
+++ 新規アプリ３/ThirdViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ThirdViewController.swift
+//  新規アプリ３
+//
+//  Created by 永田　花道 on 2025/11/10.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
